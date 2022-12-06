d0b14714269b7661d2ff2063b43af68cbc778624
sowmie21 <sssk062003@gmail.com>
2022-12-06 16:58:58 +0530

    added extra pages

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,7 +1,10 @@
+
 import 'package:flutter/material.dart';
 
 // ignore: use_key_in_widget_constructors
 class ProfileScreen extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -10,15 +13,15 @@ class ProfileScreen extends StatelessWidget {
           decoration: const BoxDecoration(
             color: Color.fromRGBO(160, 153, 238, 1),
             borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(350),
-              bottomRight: Radius.circular(350),
+              bottomLeft: Radius.circular(150),
+              bottomRight: Radius.circular(150),
             ),
             ),
             height: 400,
             width: double.infinity,
-            /*child: Column(
+            child: Column(
             // ignore: prefer_const_literals_to_create_immutables
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             // ignore: prefer_const_literals_to_create_immutables
             children: [
               const SizedBox(
@@ -26,9 +29,17 @@ class ProfileScreen extends StatelessWidget {
               ),
               Padding(
                 padding: const EdgeInsets.only(left: 10.0),
-                child: nameText,
-              ),        
-              ),*/
+                child: Text("Divyansh Mehta",
+                style: TextStyle(
+                  color: Color.fromRGBO(255, 255, 255, 1),
+                  fontFamily: "Actor",
+                  fontSize: 32,
+                ),
+                ),
+              ),   
+              Container(child: Image.asset("assets/icons/cat_profile.png",fit: BoxFit.fill,),width: 100,height: 100,)     
+            ],            
+            ),
         )
       ],
     );
